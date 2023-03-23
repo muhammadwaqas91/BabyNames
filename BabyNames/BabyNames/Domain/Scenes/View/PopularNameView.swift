3b93da695459791bf17393fdc4db463ffab97aee
Muhammad Waqas <muhammad.waqas@cheetay.pk>
2023-03-23 20:26:54 +0500

    popular name methods updates

--- BabyNames/BabyNames/Domain/Scenes/View/PopularNameView.swift
+++ BabyNames/BabyNames/Domain/Scenes/View/PopularNameView.swift
@@ -26,11 +26,76 @@ class PopularNameView: NibLoadableView, NibLoadable {
         
         if sender == maleButton {
             maleButton.isSelected = true
-            delegate?.filterBy(gender: .male)
+            vm.gender = .male
         }
         else if sender == femaleButton {
             femaleButton.isSelected = true
-            delegate?.filterBy(gender: .female)
+            vm.gender = .female
         }
+        
+        guard let gender = vm.gender else {return}
+        delegate?.filterBy(gender: gender)
+    }
+    
+    func updateUI() {
+        updateGenderUI()
+        updatePopularBabyUI()
+    }
+    
+    func updateGenderUI() {
+        maleButton.isSelected = false
+        femaleButton.isSelected = false
+        
+        switch vm.gender {
+            case .male:
+                maleButton.isSelected = true
+            case .female:
+                femaleButton.isSelected = true
+            default:
+                break
+        }
+    }
+    
+    func updatePopularBabyUI() {
+        guard let baby = vm.popularBaby?.baby else {return}
+        
+        nameLabel.text = baby.name
+        
+        let headingFont = UIFont.systemFont(ofSize: 18)
+        let headingForegroundColor: UIColor = .black
+        
+        let descriptionFont = UIFont.systemFont(ofSize: 15)
+        let descriptionForegroundColor: UIColor = .gray
+        
+        let headingAttributes: [NSAttributedString.Key : Any] = [.foregroundColor: headingForegroundColor,
+                                                                 .font: headingFont]
+        
+        let descriptionAttributes: [NSAttributedString.Key : Any] = [.foregroundColor: descriptionForegroundColor,
+                                                                     .font: descriptionFont]
+        
+        let mutableAttributedText = NSMutableAttributedString()
+        
+        
+        mutableAttributedText.append(NSAttributedString(string: "Year:" + " ", attributes: headingAttributes))
+        mutableAttributedText.append(NSAttributedString(string: "\(baby.birthYear)\n", attributes: descriptionAttributes))
+        
+        mutableAttributedText.append(NSAttributedString(string: "Ethnicity:" + " ", attributes: headingAttributes))
+        mutableAttributedText.append(NSAttributedString(string: "\(baby.ethnicity)\n", attributes: descriptionAttributes))
+        
+        mutableAttributedText.append(NSAttributedString(string: "Babies:" + " ", attributes: headingAttributes))
+        mutableAttributedText.append(NSAttributedString(string: "\(baby.noOfBabies)\n", attributes: descriptionAttributes))
+        
+        mutableAttributedText.append(NSAttributedString(string: "Rank:" + " ", attributes: headingAttributes))
+        mutableAttributedText.append(NSAttributedString(string: "\(baby.rank)\n", attributes: descriptionAttributes))
+        
+        
+        /// set paragraph style
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        mutableAttributedText.addAttribute(NSAttributedString.Key.paragraphStyle,
+                                           value:paragraphStyle,
+                                           range:NSMakeRange(0, mutableAttributedText.length))
+        
+        detailsLabel.attributedText = mutableAttributedText
     }
 }
